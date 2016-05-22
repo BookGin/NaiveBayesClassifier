0a047a340d29433b612d54f72c2ea31ae3d5d01e
Bookgin <dongsheoil@gmail.com>
2016-05-22 23:57:01 +0800

    Rename fileparser

--- src/fileparser.h
+++ src/fileparser.h
@@ -9,18 +9,18 @@
 using std::string;
 using std::vector;
 
-bool deleteChars(const char c)
+static bool deleteChars(const char c)
 {
   return not isalnum(c);
 }
 
-string& removeSymbol(string &buf)
+static string& removeSymbol(string &buf)
 {
   buf.erase(std::remove_if(buf.begin(), buf.end(), deleteChars), buf.end());
   return buf;
 }
 
-vector<string>& parse(const string& path, vector<string> &terms)
+vector<string>& parseFile(const string& path, vector<string> &terms)
 {
   std::ifstream input_file(path);
   if (!input_file) {
