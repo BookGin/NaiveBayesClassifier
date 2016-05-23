d15c7b82d55eb8e7a3b33cc254a7be869626ced5
Bookgin <dongsheoil@gmail.com>
2016-05-23 21:52:54 +0800

    Add check two ans script

---
+++ src/script/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 2 ];then 
+  echo 'Incorrect arguments'
+  exit 1 
+fi
+
+error=`diff $1 $2 | grep '^>' | wc -l`
+all=`cat $1 | wc -l`
+error_rate=`echo "scale=2;$error/$all*100" | bc`
+echo "Wrong: $error/$all"
+echo "Error rate: $error_rate%"
