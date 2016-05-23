2c5e07018c5082c464a2f9ddb762aad1b99948ed
Bookgin <dongsheoil@gmail.com>
2016-05-23 22:33:09 +0800

    Fix check accuracy bug

--- src/script/check.sh
+++ src/script/check.sh
@@ -7,6 +7,6 @@ fi
 
 error=`diff $1 $2 | grep '^>' | wc -l`
 all=`cat $1 | wc -l`
-error_rate=`echo "scale=2;$error/$all*100" | bc`
+error_rate=`echo "scale=4;$error/$all*100.0" | bc`
 echo "Wrong: $error/$all"
 echo "Error rate: $error_rate%"
