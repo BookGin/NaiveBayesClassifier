e2fac333014b40becbad151b95026d37786f6406
Bookgin <dongsheoil@gmail.com>
2016-06-02 10:39:37 +0800

    Finish naivebayes program

---
+++ src/naivebayes.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+data_dir=""
+output_file=""
+label_size=""
+
+while getopts "n:i:o:" opt; do
+    case "$opt" in
+    n)  label_size=$OPTARG
+        ;;
+    i)  data_dir=$OPTARG
+        ;;
+    o)  output_file=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+echo "data_dir='$data_dir', output_file='$output_file', label_size=$label_size"
+
+label_path=./tmpdir/label-filelist
+unlabel_path=./tmpdir/unlabel-filelist
+query_path=./tmpdir/querylist
+
+mkdir ./tmpdir/
+./script/gen-label-file-list.sh $data_dir/Train $label_size > $label_path
+./script/gen-filelist.sh $data_dir/Unlabel > $unlabel_path
+./script/gen-filelist.sh $data_dir/Test > $query_path
+
+./naivebayes $label_path $query_path > $output_file 
+
+rm -rf ./tmpdir
