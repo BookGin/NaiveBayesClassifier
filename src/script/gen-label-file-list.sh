5bb08cb311239981c542fbcf83dd27ccec9c8673
Bookgin <dongsheoil@gmail.com>
2016-06-02 10:34:24 +0800

    Add a arguments in gen label file

--- src/script/gen-label-file-list.sh
+++ src/script/gen-label-file-list.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];then 
+if [ $# -ne 1 ];then 
   echo 'Incorrect arguments'
   exit 1 
 fi
 
 for topic in `ls $1`;do
   echo ":$topic"
-  for file in `ls "$1/$topic" | head -n $2`;do
-    echo "$1/$topic/$file"
-  done
+  if [ "$2" == "" ]; then
+    for file in `ls "$1/$topic"`;do
+      echo "$1/$topic/$file"
+    done
+  else
+    for file in `ls "$1/$topic" | head -n $2`;do
+      echo "$1/$topic/$file"
+    done
+  fi
 done
 
